d46cc692c1e3c529beb65688f7dc548f901ab7ab
anupasin <birupia@gmail.com>
2024-06-19 21:50:25 +0530

    fixed error in about.js

--- pages/about.js
+++ pages/about.js
@@ -6,9 +6,9 @@ const About = () => {
     <div className="about-container">
       <Navbar />
       <div className="about-content">
-        <div className="about-text" style={{ textAlign: "left" }}>
+        <div className="about-text">
           <h1 className="about-heading-lg">Welcome to Earth Family Resort</h1>
-          <h2 className="about-heading-sm">Your Sanctuary in Nature's Embrace</h2>
+          <h2 className="about-heading-sm">Your Sanctuary in Nature&apos;s Embrace</h2>
           <p className="about-paragraph">
             Nestled in the heart of breathtaking natural beauty, Earth Family Resort offers an idyllic retreat for families and nature enthusiasts alike. Our eco-friendly resort is designed to harmonize with the surrounding environment, providing luxurious accommodations and amenities without compromising our commitment to sustainability. From serene forest trails to rejuvenating spa experiences, every aspect of Earth Family Resort is crafted to reconnect you with nature and create unforgettable memories with your loved ones. Join us in celebrating the Earth and all its wonders, as you relax and recharge in our tranquil haven.
           </p>
